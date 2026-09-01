75d00adce00c21ff4d369df37b32a175dc3ab4dd
khushisaini2709 <khushisaini8634@gmail.com>
2026-09-01 10:18:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const prodContainer = document.getElementById("product-container");
+let products = [];
 
-const app = React.createElement(
-  'div',
-  null,
-  React.createElement('h1', null, 'E-commerce webpage'),
-  React.createElement(
-    'div',
-    { id: 'inner-div-01' },
-    React.createElement('span', null, '01'),
-    React.createElement('span', null, '02')
-  ),
-  React.createElement(
-    'div',
-    { id: 'inner-div-02' },
-    React.createElement('span', null, '01'),
-    React.createElement('span', null, '02')
-  )
-);
+const showProducts = async () => {
+    const res = await fetch("https://dummyjson.com/products");
+    const data = await res.json();
+    products = data.products;
 
-root.render(app);
+    products.forEach((product) => {
+        const div = document.createElement("div");
+
+        const img = document.createElement("img");
+        img.src = product.thumbnail;
+        img.alt = product.title;
+
+        const title = document.createElement("h2");
+        title.innerText = product.title;
+
+        const price = document.createElement("p");
+        price.innerText = `$${product.price}`;
+
+        const decrementBtn = document.createElement("button");
+        decrementBtn.innerText = "-";
+
+        const span = document.createElement("span");
+        span.innerText = "0";
+
+        const incrementBtn = document.createElement("button");
+        incrementBtn.innerText = "+";
+
+        let counter = 0;
+
+        incrementBtn.addEventListener("click", () => {
+            counter++;
+            span.innerText = counter;
+        });
+
+        decrementBtn.addEventListener("click", () => {
+            if (counter > 0) {
+                counter--;
+                span.innerText = counter;
+            }
+        });
+
+        div.append(img, title, price, decrementBtn, span, incrementBtn);
+        prodContainer.appendChild(div);
+    });
+};
+
+showProducts();
